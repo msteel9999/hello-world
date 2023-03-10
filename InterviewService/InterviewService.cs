3ee6237205e734dc904e4a29745d3fb78471f7d4
Martin Steel <martin.steel@barnett-waddingham.co.uk>
2023-03-10 22:26:16 +0000

    TES|T

--- InterviewService/InterviewService.cs
+++ InterviewService/InterviewService.cs
@@ -1,14 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Interview.Services
 {
     public class InterviewService
     {
+        //Task 1
         public String SimpleReverse(String input)
         {
-            // Replace the next line of code with an implementation of Task 1.
-            // NOTE: As soon as this line is removed the sample unit test in (SimpleReverseTests.cs) will fail and can be removed.
-            throw new NotImplementedException("Implement me first!");
+            var inputWords = input.Split(' ');
+            Array.Reverse(inputWords);
+            return string.Join(" ", inputWords);
+        }
+
+        //Task 2
+        public double GetAverageEvenNumberOfObjects(List<object> objects)
+        {
+            var count = 0;
+            var sum = 0.0;
+
+            foreach (var obj in objects)
+            {
+                if (!(obj is int)) continue;
+
+                var number = (int)obj;
+                if (number % 2 != 0) continue;
+                count++;
+                sum += number;
+            }
+
+            return count == 0 ? 0.0 : sum / count;
+        }
+
+        //Task 3
+        public string ReverseWithPunctuation(string input)
+        {
+            var wordsAndPunctuation = Regex.Split(input, @"(\W)");
+            Array.Reverse(wordsAndPunctuation);
+
+            var sb = new StringBuilder();
+            var capitaliseNext = false;
+
+            foreach (var s in wordsAndPunctuation)
+            {
+                var changedS = s;
+                if (capitaliseNext && changedS.Length > 0)
+                {
+                    changedS = changedS[0].ToString().ToUpper();
+                    if(s.Length > 1)
+                    {
+                        changedS += s.Substring(1);
+                    }      
+                    capitaliseNext = false;
+                }
+
+                if (IsPunctuation(s)) 
+                {
+                    capitaliseNext = true;
+                }
+
+                sb.Append(changedS);
+            }
+
+            return sb.ToString();
+        }
+
+        private static bool IsPunctuation(string s)
+        {
+            return s.Length == 1 && !char.IsLetterOrDigit(s[0]) && !string.IsNullOrWhiteSpace(s);
         }
     }
 }
--- InterviewTests/SimpleReverseTests.cs
+++ InterviewTests/SimpleReverseTests.cs
@@ -14,11 +14,31 @@ namespace Interview.UnitTests.Service
         }
 
         [Fact]
-        // This test is a sample test and should be removed!
-        public void SampleTestThrowsNotImplementedException()
+        // Task 1
+        public void When_GivenASentence_SimpleRevers_ReturnsReversedString()
         {
-            Action a = () => _interviewService.SimpleReverse("Test");
-            Assert.Throws<NotImplementedException>(a);
+            var actual = _interviewService.SimpleReverse("This is a computer");
+            var expected = "computer a is This";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        //Task 2
+        public void When_ArrayWithObjectsAndInts_GetAverageEvenNumberOfObjectsTest_ReturnsExpectedValue()
+        {
+            var stringArray = new String[] { "tea", "coffee" };
+            var actual = _interviewService.GetAverageEvenNumberOfObjects(new System.Collections.Generic.List<object> { 1, 2, 3, 4, stringArray, 6, 7, 8, 9, 10 });
+            var expected = 6.0;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        //Task 3
+        public void When_PunctuatedSentence_ReverseWithPunctuation_ReturnsReversedWithPunctuation()
+        {
+            var actual = _interviewService.ReverseWithPunctuation("Are you there? I am here.");
+            var expected = ".Here am i ?There you are";
+            Assert.Equal(expected, actual);
         }
     }
 }
